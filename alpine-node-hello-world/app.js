85eb78c9606d47e8a05159901d5eeb32d0baaa2b
Fabio Romanini <fabioaromanini@gmail.com>
2018-05-31 16:29:00 -0300

    adds identifier to alpinetest instance on html reponse

--- alpine-node-hello-world/app.js
+++ alpine-node-hello-world/app.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
+var random = ((Math.random() * 10000) % 100) + 1;
+
 http.createServer(function(req,res) {
   res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('Hello World!');
+  res.end('Hello World! ' + parseInt(random));
 }).listen(8080);
